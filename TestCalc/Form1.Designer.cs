5a9d4a51b771f9673610050a978a5bac44f4f96c
sharp_NEKO <65962822+sharp-NEKO@users.noreply.github.com>
2020-07-24 03:02:38 +0900

    Add files via upload

---
+++ TestCalc/Form1.Designer.cs
@@ -0,0 +1,316 @@
+﻿namespace TestCalc
+{
+    partial class CalcForm
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CalcForm));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Ja_MeBox = new System.Windows.Forms.TextBox();
+            this.Ja_AverBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Ja_PRText = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.Ma_MeBox = new System.Windows.Forms.TextBox();
+            this.Ma_AverBox = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.Sc_MeBox = new System.Windows.Forms.TextBox();
+            this.Sc_AverBox = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.So_MeBox = new System.Windows.Forms.TextBox();
+            this.So_AverBox = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.En_MeBox = new System.Windows.Forms.TextBox();
+            this.En_AverBox = new System.Windows.Forms.TextBox();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.En_PRText = new System.Windows.Forms.Label();
+            this.Ma_PRText = new System.Windows.Forms.Label();
+            this.Sc_PRText = new System.Windows.Forms.Label();
+            this.So_PRText = new System.Windows.Forms.Label();
+            this.Total_MeText = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.Total_AverText = new System.Windows.Forms.Label();
+            this.Button = new System.Windows.Forms.Button();
+            this.Ver = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.Exit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // Ja_MeBox
+            // 
+            resources.ApplyResources(this.Ja_MeBox, "Ja_MeBox");
+            this.Ja_MeBox.Name = "Ja_MeBox";
+            // 
+            // Ja_AverBox
+            // 
+            resources.ApplyResources(this.Ja_AverBox, "Ja_AverBox");
+            this.Ja_AverBox.Name = "Ja_AverBox";
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.ForeColor = System.Drawing.Color.Blue;
+            this.label3.Name = "label3";
+            // 
+            // Ja_PRText
+            // 
+            resources.ApplyResources(this.Ja_PRText, "Ja_PRText");
+            this.Ja_PRText.ForeColor = System.Drawing.Color.Blue;
+            this.Ja_PRText.Name = "Ja_PRText";
+            // 
+            // label5
+            // 
+            resources.ApplyResources(this.label5, "label5");
+            this.label5.Name = "label5";
+            // 
+            // Ma_MeBox
+            // 
+            resources.ApplyResources(this.Ma_MeBox, "Ma_MeBox");
+            this.Ma_MeBox.Name = "Ma_MeBox";
+            // 
+            // Ma_AverBox
+            // 
+            resources.ApplyResources(this.Ma_AverBox, "Ma_AverBox");
+            this.Ma_AverBox.Name = "Ma_AverBox";
+            // 
+            // label6
+            // 
+            resources.ApplyResources(this.label6, "label6");
+            this.label6.Name = "label6";
+            // 
+            // Sc_MeBox
+            // 
+            resources.ApplyResources(this.Sc_MeBox, "Sc_MeBox");
+            this.Sc_MeBox.Name = "Sc_MeBox";
+            // 
+            // Sc_AverBox
+            // 
+            resources.ApplyResources(this.Sc_AverBox, "Sc_AverBox");
+            this.Sc_AverBox.Name = "Sc_AverBox";
+            // 
+            // label8
+            // 
+            resources.ApplyResources(this.label8, "label8");
+            this.label8.Name = "label8";
+            // 
+            // So_MeBox
+            // 
+            resources.ApplyResources(this.So_MeBox, "So_MeBox");
+            this.So_MeBox.Name = "So_MeBox";
+            // 
+            // So_AverBox
+            // 
+            resources.ApplyResources(this.So_AverBox, "So_AverBox");
+            this.So_AverBox.Name = "So_AverBox";
+            // 
+            // label10
+            // 
+            resources.ApplyResources(this.label10, "label10");
+            this.label10.Name = "label10";
+            // 
+            // En_MeBox
+            // 
+            resources.ApplyResources(this.En_MeBox, "En_MeBox");
+            this.En_MeBox.Name = "En_MeBox";
+            // 
+            // En_AverBox
+            // 
+            resources.ApplyResources(this.En_AverBox, "En_AverBox");
+            this.En_AverBox.Name = "En_AverBox";
+            // 
+            // label14
+            // 
+            resources.ApplyResources(this.label14, "label14");
+            this.label14.Name = "label14";
+            // 
+            // label4
+            // 
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.ForeColor = System.Drawing.Color.Red;
+            this.label4.Name = "label4";
+            // 
+            // En_PRText
+            // 
+            resources.ApplyResources(this.En_PRText, "En_PRText");
+            this.En_PRText.ForeColor = System.Drawing.Color.Blue;
+            this.En_PRText.Name = "En_PRText";
+            // 
+            // Ma_PRText
+            // 
+            resources.ApplyResources(this.Ma_PRText, "Ma_PRText");
+            this.Ma_PRText.ForeColor = System.Drawing.Color.Blue;
+            this.Ma_PRText.Name = "Ma_PRText";
+            // 
+            // Sc_PRText
+            // 
+            resources.ApplyResources(this.Sc_PRText, "Sc_PRText");
+            this.Sc_PRText.ForeColor = System.Drawing.Color.Blue;
+            this.Sc_PRText.Name = "Sc_PRText";
+            // 
+            // So_PRText
+            // 
+            resources.ApplyResources(this.So_PRText, "So_PRText");
+            this.So_PRText.ForeColor = System.Drawing.Color.Blue;
+            this.So_PRText.Name = "So_PRText";
+            // 
+            // Total_MeText
+            // 
+            resources.ApplyResources(this.Total_MeText, "Total_MeText");
+            this.Total_MeText.ForeColor = System.Drawing.Color.Red;
+            this.Total_MeText.Name = "Total_MeText";
+            // 
+            // label7
+            // 
+            resources.ApplyResources(this.label7, "label7");
+            this.label7.ForeColor = System.Drawing.Color.Red;
+            this.label7.Name = "label7";
+            // 
+            // Total_AverText
+            // 
+            resources.ApplyResources(this.Total_AverText, "Total_AverText");
+            this.Total_AverText.ForeColor = System.Drawing.Color.Red;
+            this.Total_AverText.Name = "Total_AverText";
+            // 
+            // Button
+            // 
+            resources.ApplyResources(this.Button, "Button");
+            this.Button.Name = "Button";
+            this.Button.UseVisualStyleBackColor = true;
+            this.Button.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // Ver
+            // 
+            resources.ApplyResources(this.Ver, "Ver");
+            this.Ver.Name = "Ver";
+            // 
+            // label9
+            // 
+            resources.ApplyResources(this.label9, "label9");
+            this.label9.Name = "label9";
+            // 
+            // Exit
+            // 
+            this.Exit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            resources.ApplyResources(this.Exit, "Exit");
+            this.Exit.Name = "Exit";
+            this.Exit.UseVisualStyleBackColor = true;
+            // 
+            // CalcForm
+            // 
+            this.AcceptButton = this.Button;
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.Exit;
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.Ver);
+            this.Controls.Add(this.Exit);
+            this.Controls.Add(this.Button);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.Total_AverText);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.Total_MeText);
+            this.Controls.Add(this.So_PRText);
+            this.Controls.Add(this.Sc_PRText);
+            this.Controls.Add(this.Ma_PRText);
+            this.Controls.Add(this.En_PRText);
+            this.Controls.Add(this.Ja_PRText);
+            this.Controls.Add(this.En_AverBox);
+            this.Controls.Add(this.En_MeBox);
+            this.Controls.Add(this.So_AverBox);
+            this.Controls.Add(this.So_MeBox);
+            this.Controls.Add(this.Sc_AverBox);
+            this.Controls.Add(this.Sc_MeBox);
+            this.Controls.Add(this.Ma_AverBox);
+            this.Controls.Add(this.Ma_MeBox);
+            this.Controls.Add(this.Ja_AverBox);
+            this.Controls.Add(this.Ja_MeBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "CalcForm";
+            this.Load += new System.EventHandler(this.CalcForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox Ja_MeBox;
+        private System.Windows.Forms.TextBox Ja_AverBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label Ja_PRText;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox Ma_MeBox;
+        private System.Windows.Forms.TextBox Ma_AverBox;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox Sc_MeBox;
+        private System.Windows.Forms.TextBox Sc_AverBox;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox So_MeBox;
+        private System.Windows.Forms.TextBox So_AverBox;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox En_MeBox;
+        private System.Windows.Forms.TextBox En_AverBox;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label En_PRText;
+        private System.Windows.Forms.Label Ma_PRText;
+        private System.Windows.Forms.Label Sc_PRText;
+        private System.Windows.Forms.Label So_PRText;
+        private System.Windows.Forms.Label Total_MeText;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label Total_AverText;
+        private System.Windows.Forms.Button Button;
+        private System.Windows.Forms.Label Ver;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button Exit;
+    }
+}
+
---
+++ TestCalc/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestCalc
+{
+    public partial class CalcForm : Form
+    {
+        public CalcForm()
+        {
+            InitializeComponent();
+        }
+
+
+        public void CalcForm_Load(object sender, EventArgs e)
+        {
+            // By #NEKO
+            // http://www.NEKOnsole.com
+            // Ver2.0.0-R
+        }
+
+        public void Button_Click(object sender, EventArgs e)
+        {
+            // 教科ごとの点数
+            int Ja_Me, En_Me, Ma_Me, Sc_Me, So_Me;
+            int Ja_Aver, En_Aver, Ma_Aver, Sc_Aver, So_Aver;
+
+            // 結果
+            int Total_Me, Total_Aver;
+
+            // 点数の変数化
+            Ja_Me               = Convert.ToInt32(Ja_MeBox.Text);
+            En_Me               = Convert.ToInt32(En_MeBox.Text);
+            Ma_Me               = Convert.ToInt32(Ma_MeBox.Text);
+            Sc_Me               = Convert.ToInt32(Sc_MeBox.Text);
+            So_Me               = Convert.ToInt32(So_MeBox.Text);
+
+            // 平均の変数化
+            Ja_Aver             = Convert.ToInt32(Ja_AverBox.Text);
+            En_Aver             = Convert.ToInt32(En_AverBox.Text);
+            Ma_Aver             = Convert.ToInt32(Ma_AverBox.Text);
+            Sc_Aver             = Convert.ToInt32(Sc_AverBox.Text);
+            So_Aver             = Convert.ToInt32(So_AverBox.Text);
+
+            // 結果の変数化と計算
+            Total_Me            = Ja_Me + En_Me + Ma_Me + Sc_Me + So_Me;
+            Total_Aver          = Ja_Aver + En_Aver + Ma_Aver + Sc_Aver + So_Aver;
+
+            // 結果を出力
+            Ja_PRText.Text      = Convert.ToString(50 + (Ja_Me - Ja_Aver) / 2);
+            En_PRText.Text      = Convert.ToString(50 + (En_Me - En_Aver) / 2);
+            Ma_PRText.Text      = Convert.ToString(50 + (Ma_Me - Ma_Aver) / 2);
+            Sc_PRText.Text      = Convert.ToString(50 + (Sc_Me - Sc_Aver) / 2);
+            So_PRText.Text      = Convert.ToString(50 + (So_Me - So_Aver) / 2);
+
+            Total_MeText.Text   = Convert.ToString(Total_Me);
+            Total_AverText.Text = Convert.ToString(Total_Aver);
+        }
+
+        private void Exit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
